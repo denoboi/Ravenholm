df13b3ac6dbdde291df4258c0f2349f7d916abbc
denoboi <deniz.kaymakci@hotmail.com>
2021-08-11 22:16:45 +0300

    Animation beginning

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        if(hitPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
