3803b6c02e9b58261255b511dc3847afa1dffec3
denoboi <deniz.kaymakci@hotmail.com>
2021-08-16 22:01:52 +0300

    LookTarget(Enemy Looking)

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -7,6 +7,7 @@ public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;
+    [SerializeField] float turnSpeed = 6f;
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget;
@@ -57,6 +58,7 @@ public class EnemyAI : MonoBehaviour
     }
     void EngageTarget()
     {
+        FaceTarget();
         // stopping distance player'in yanina gelme uzakligi. 
         // stopping distance'i 1 yaparsam 1 birim yanina gelene kadar takip edecek.
 
@@ -74,10 +76,19 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
+
    
     void AttackTarget()
     {
         GetComponent<Animator>().SetBool("Attack",true);
         Debug.Log(name + "has seeked and is destroying" + target.name);
+    }
+
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+
     }
 }
