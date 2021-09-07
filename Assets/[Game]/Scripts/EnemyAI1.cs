d4418a4c3c0fb80e1424aa1ea71c06f0d58a866c
denoboi <deniz.kaymakci@hotmail.com>
2021-09-07 23:33:02 +0300

    AI3

--- Assets/[Game]/Scripts/EnemyAI1.cs
+++ Assets/[Game]/Scripts/EnemyAI1.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,28 +12,64 @@ public class EnemyAI1 : MonoBehaviour
     NavMeshAgent navMeshAgent;
 
     // enemy kendisinden uzakta oldugunu anlamasi icin, infinity yapmazsak default 0 oluyor bu da hemen ustunde ya da yaninda gibi, o da sacmaliyor.
-    float distanceToTarget = Mathf.Infinity; 
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    
+
     void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        
-
-        if(distanceToTarget <= chaseRange)
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
         }
+
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+
+
     }
+
+    void ChaseTarget()
+    {
+        GetComponent<Animator>().SetTrigger("Move");
+        navMeshAgent.SetDestination(target.position);
+    }    
+
+    void AttackTarget()
+    {
+        GetComponent<Animator>().SetBool("Attack", true);
+        Debug.Log(name + "has seeked and is destroying" + target.name);
+    }
+
+        void EngageTarget()
+        {
+            if (distanceToTarget >= navMeshAgent.stoppingDistance)
+            {
+                ChaseTarget();
+            }
+
+            if (distanceToTarget <= navMeshAgent.stoppingDistance)
+            {
+                AttackTarget();
+
+            }
+        }
 
     void OnDrawGizmosSelected()
     {
-        // Display the chase radius when selected
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, chaseRange);
+     // Display the chase radius when selected
+     Gizmos.color = Color.red;
+     Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
+
 }
+
+
