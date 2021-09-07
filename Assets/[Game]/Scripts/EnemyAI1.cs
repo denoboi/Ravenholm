dc26d8fb3a6fa0d31ea7f75cf48f5c1f55478d32
denoboi <deniz.kaymakci@hotmail.com>
2021-09-07 21:58:05 +0300

    Enemy Ai1

---
+++ Assets/[Game]/Scripts/EnemyAI1.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI1 : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent navMeshAgent;
+
+    // enemy kendisinden uzakta oldugunu anlamasi icin, infinity yapmazsak default 0 oluyor bu da hemen ustunde ya da yaninda gibi, o da sacmaliyor.
+    float distanceToTarget = Mathf.Infinity; 
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        
+
+        if(distanceToTarget <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+    }
+}
