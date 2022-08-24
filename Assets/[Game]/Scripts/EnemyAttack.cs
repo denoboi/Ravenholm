da7c5f273f19714b511fe810fd4ddd3fe8736088
Deniz Kaymakci <deniz.kaymakci@hotmail.com>
2022-08-24 22:19:53 +0300

    Update EnemyAttack.cs

--- Assets/[Game]/Scripts/EnemyAttack.cs
+++ Assets/[Game]/Scripts/EnemyAttack.cs
@@ -8,16 +8,7 @@ public class EnemyAttack : MonoBehaviour
 
     [SerializeField] float damage = 20f;
 
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+  
 
     public void AttackHitEvent()
     {
